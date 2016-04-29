5d5818f6f4bdcf3aafba9fb290f372f714950f7a
= <=>
2016-04-29 23:13:55 +0300

    added dynamic time and nr of tests from input:

--- test_alg/app/views.py
+++ test_alg/app/views.py
@@ -2,31 +2,55 @@ from app import app
 from forms import testForm
 from flask import render_template, request, redirect
 import random
+
+import os
+import timeit
+import resource 
 # from app.forms import
 
+NR_OF_TESTS = 10
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
 	update = 'False'
-	if update == 'False':
-		algorithm =['BMS', 'AES']
+	form = testForm(request.form, csrf_enabled=True)
+
+	algorithm = 'BMS'
 	msg_len = 10
 	key_len = 10
-	enc_time = 0.3
-	dec_time = 0.1
+	enc_time = []
+	dec_time = []
+
+
+	nr_of_tests = 0
 
 	if request.method == 'POST':
+		nr_of_tests = int(form.nr_tests.data)
+		print "nr of tests:", nr_of_tests
 		update = 'True'
 		print "posted"
 		# algorithm[0] = "BMS1"
 		msg_len *= random.randint(1, 10)
 		key_len += 1
-		enc_time += 0.001
-		dec_time += 0.0001
-		# algorithm.append('Algorithm')
+		
+		for i in range(nr_of_tests):
+			temp_enc_time =(timeit.timeit("funct()", setup="from app.views import funct", number=1)) 
+			print "time:", (timeit.timeit("funct()", setup="from app.views import funct", number=1)) ,  "seconds"
+			# print "memory:" , resource.getrusage(resource.RUSAGE_SELF).ru_maxrss, "bytes"
+			enc_time.append(temp_enc_time)  
+			# !!!!!!!!!!!!!!!!!!!!!!
+			dec_time.append(temp_enc_time)  
+
+			# algorithm.append('Algorithm')
 
 
 
-	form = testForm(request.form, csrf_enabled=True)
+	
 	return render_template("index.html", form=form, update=update, algorithm=algorithm, msg_len=msg_len, 
-			key_len=key_len, enc_time=enc_time, dec_time=dec_time)
+			key_len=key_len, enc_time=enc_time, dec_time=dec_time, nr_of_tests=nr_of_tests)
+
+
+def funct():
+	a = 2+2
+	print "memory:" , resource.getrusage(resource.RUSAGE_SELF).ru_maxrss, "bytes"
+	
