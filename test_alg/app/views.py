7276088fccc518522d57e58f4bc538ff64849261
= <=>
2016-04-29 23:52:02 +0300

    added chart.html with values, still need to fix floating point on chart

--- test_alg/app/views.py
+++ test_alg/app/views.py
@@ -34,14 +34,19 @@ def index():
 		key_len += 1
 		
 		for i in range(nr_of_tests):
+			print "hello"
 			temp_enc_time =(timeit.timeit("funct()", setup="from app.views import funct", number=1)) 
-			print "time:", (timeit.timeit("funct()", setup="from app.views import funct", number=1)) ,  "seconds"
+			
 			# print "memory:" , resource.getrusage(resource.RUSAGE_SELF).ru_maxrss, "bytes"
-			enc_time.append(temp_enc_time)  
+			enc_time.append(int(temp_enc_time*1000000))  
+			print "time:",int(temp_enc_time*1000000),  "seconds"
 			# !!!!!!!!!!!!!!!!!!!!!!
-			dec_time.append(temp_enc_time)  
+			dec_time.append(50 + temp_enc_time*100000+20)  
 
 			# algorithm.append('Algorithm')
+		
+		return render_template("chart.html", form=form, update=update, algorithm=algorithm, msg_len=msg_len, 
+			key_len=key_len, enc_time=enc_time, dec_time=dec_time, nr_of_tests=nr_of_tests)
 
 
 
