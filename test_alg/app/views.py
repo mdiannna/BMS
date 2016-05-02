119bd0a13c37afcb68f27f313e62f3453e01bcde
= <=>
2016-05-02 11:03:52 +0300

    cleaning the code + functions

--- test_alg/app/views.py
+++ test_alg/app/views.py
@@ -12,6 +12,7 @@ from itertools import groupby
 # from app.forms import
 
 NR_OF_TESTS = 10
+test_args = ""
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
@@ -44,41 +45,22 @@ def index():
 			nr_of_tests = 0
 			show_chart = False
 
-		print "nr of tests:", nr_of_tests
-		
-		# algorithm[0] = "BMS1"
 		msg_len *= random.randint(1, 10)
 		key_len += 1
 
-		enc_time_sum = 0
-		
-		
-		for i in range(nr_of_tests):
-			temp_enc_time =(timeit.timeit("funct()", setup="from app.views import funct", number=1)) 
-			enc_time_sum += temp_enc_time
-			temp_enc_time *= 1000
-			# print "memory:" , resource.getrusage(resource.RUSAGE_SELF).ru_maxrss, "bytes"
-			enc_time.append(temp_enc_time)  
-			# !!!!!!!!!!!!!!!!!!!!!!
-			dec_time.append("Not computed")  
 
-			# algorithm.append('Algorithm')
-		for i in range(nr_of_tests):
-			enc_time_median.append(median(enc_time))
-			enc_time_avg.append(mean(enc_time))
-			
-		enc_time_int = map(int, enc_time)
-		print "MODE:", calcMode(enc_time_int)
-		enc_mode = mean(calcMode(enc_time_int))
-
-		for i in range(nr_of_tests):
-			enc_time_mode.append(enc_mode);
-
+		enc_dec = "enc"
+		data_statistics = calcPerformance(enc_dec, key_len, msg_len, algorithm, nr_of_tests)
+		enc_time = data_statistics[0]
+		enc_time_median = data_statistics[1]
+		enc_time_avg = data_statistics[2]
+		enc_time_mode = data_statistics[3]
 	
 	return render_template("index.html", form=form, algorithm=algorithm, msg_len=msg_len, 
 			key_len=key_len, enc_time=enc_time, dec_time=dec_time, nr_of_tests=nr_of_tests, 
 			enc_time_median=enc_time_median, enc_time_avg=enc_time_avg, enc_time_mode=enc_time_mode,
 			exception=exception, show_chart=show_chart)
+
 
 def calcMode(list):	
 	d = {}
@@ -110,9 +92,64 @@ def calcMode(list):
 	return max_k
 
 
-
-def funct():
+def test_enc_BMS(args):
 	# a = 2+2
-	os.system("python test.py 10 asd")
+	os.system("python test.py " + args)
 	print "memory:" , resource.getrusage(resource.RUSAGE_SELF).ru_maxrss, "bytes"
+
+def test_dec_BMS(args):
+	# a = 2+2
+	os.system("python test.py " + args)
+	print "memory:" , resource.getrusage(resource.RUSAGE_SELF).ru_maxrss, "bytes"
+
+def test_enc_AES(args):
+	# a = 2+2
+	os.system("python test.py " + args)
+	print "memory:" , resource.getrusage(resource.RUSAGE_SELF).ru_maxrss, "bytes"
+
+def test_dec_AES(args):
+	# a = 2+2
+	os.system("python test.py " + args)
+	print "memory:" , resource.getrusage(resource.RUSAGE_SELF).ru_maxrss, "bytes"
+
+
+def calcPerformance(enc_dec, key_len, msg_len, algorithm, nr_of_tests):
+
+	msg = "test"
+	key = 1234
+
+	global test_args 
+	test_args = " " + str(msg) + " " + str(key)			
+	function_name = "test_" + enc_dec + "_" + algorithm
+
+	enc_time = []
+	dec_time = []
+	enc_time_median = []
+	enc_time_avg = []
+	enc_time_mode = []
+
+	for i in range(nr_of_tests):
+		temp_enc_time =(timeit.timeit(function_name + "(test_args)", setup="from app.views import " + function_name + "," + "test_args", number=1)) 
+		temp_enc_time *= 1000
+		# print "memory:" , resource.getrusage(resource.RUSAGE_SELF).ru_maxrss, "bytes"
+		enc_time.append(temp_enc_time)  
+		# !!!!!!!!!!!!!!!!!!!!!!
+		dec_time.append("Not computed")  
+
+		# algorithm.append('Algorithm')
+	for i in range(nr_of_tests):
+		enc_time_median.append(median(enc_time))
+		enc_time_avg.append(mean(enc_time))
+		
+	enc_time_int = map(int, enc_time)
+	print "MODE:", calcMode(enc_time_int)
+	enc_mode = mean(calcMode(enc_time_int))
+
+	for i in range(nr_of_tests):
+		enc_time_mode.append(enc_mode);
+
+	return enc_time, enc_time_median, enc_time_avg, enc_time_mode
+
+
+
 	
