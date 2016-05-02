eaf974329ecd9d5e9ca700f30dd52d84a2c6b716
= <=>
2016-05-02 10:32:36 +0300

    deleted some comments

--- test_alg/app/views.py
+++ test_alg/app/views.py
@@ -54,13 +54,11 @@ def index():
 		
 		
 		for i in range(nr_of_tests):
-			# print "hello"
 			temp_enc_time =(timeit.timeit("funct()", setup="from app.views import funct", number=1)) 
 			enc_time_sum += temp_enc_time
 			temp_enc_time *= 1000
 			# print "memory:" , resource.getrusage(resource.RUSAGE_SELF).ru_maxrss, "bytes"
 			enc_time.append(temp_enc_time)  
-			# # print "time:",temp_enc_time,  "seconds"
 			# !!!!!!!!!!!!!!!!!!!!!!
 			dec_time.append("Not computed")  
 
@@ -68,34 +66,13 @@ def index():
 		for i in range(nr_of_tests):
 			enc_time_median.append(median(enc_time))
 			enc_time_avg.append(mean(enc_time))
-			# enc_time_mode.append(mode(enc_time))
-			# enc_time_mode = Counter(enc_time).most_common()
-
-		# data = Counter(enc_time)
-		# data.most_common()   # Returns all unique items and their counts
-		# temp = data.most_common(1)
-		# mode_sum = 0
-
-		# for j in range(len(temp)):
-		# 	temp2 = temp[j]
-		# 	# print "tem[2:", temp2
-		# 	temp3 = temp2[j]
-		# 	mode_sum += temp3;
-		# 	# print "tem[3:", temp3
-		# enc_mode = mode_sum/len(temp)
-
+			
 		enc_time_int = map(int, enc_time)
 		print "MODE:", calcMode(enc_time_int)
 		enc_mode = mean(calcMode(enc_time_int))
 
 		for i in range(nr_of_tests):
 			enc_time_mode.append(enc_mode);
-
-		
-		# return render_template("chart2.html", form=form, algorithm=algorithm, msg_len=msg_len, 
-		# 	key_len=key_len, enc_time=enc_time, dec_time=dec_time, nr_of_tests=nr_of_tests)
-
-
 
 	
 	return render_template("index.html", form=form, algorithm=algorithm, msg_len=msg_len, 
