3a090edff17b877121e5897821fdb5c2e9f5e7b6
= <=>
2016-05-03 22:48:48 +0300

    Errors in encryption&decryption c++

--- test_alg/BMS_cpp/main.cpp
+++ test_alg/BMS_cpp/main.cpp
@@ -16,45 +16,57 @@ arr K1;
 int first_key_len, block_pos;
 
 
-int main(){
-    freopen("test.in", "r", stdin);
-    //freopen("test.out", "w", stdout);
-
-    clock_t tStart = clock();
-    srand (time(NULL));
-
-    string plaintext= "";
-    string ciphertext = "";
-    int block_size = 3;
-    int key_len = 0;
-    int int_key;
-    arr K1;
+int main(int argc, char *argv[]){
+    cout << "AAAAAA";
+    if(argc <3)
+        cout << "Error! please specify message and key";
+    else{
 
 
-    scanf("%d", &int_key);
-    // printf("Key: %d\n", int_key);
+        clock_t tStart = clock();
+        srand (time(NULL));
 
-    readKey(int_key, K1, key_len);
-    scanf("%d" , &block_size);
-    // printf("bLOCK SIZE: %d\n", block_size);
+        string plaintext= "";
+        string ciphertext = "";
+        int block_size = 3;
+        int key_len = 0;
+        int int_key;
+        arr K1;
 
-    cin >> plaintext;
-   ////// cout << "plaintext length:" << plaintext.length();
-  // cout << "Plaintext:" << plaintext << endl;
-    //printf("Plaintext: %s\n", plaintext);
+        // cout << "argc:" << argc;
+        // cout << "argumens:" << argv[1] << argv[1];
 
-    ciphertext = blockEncryptionV4 ( block_size ,  plaintext , K1, key_len);
-  //   cout << "Ciphertext: " <<  ciphertext << endl;
+        // plaintext = argv[1];
+        // int_key = atoi(argv[2]);
+        plaintext = "aaass";
+        int_key = 5343;
+///        // scanf("%d", &int_key);
+        // printf("Key: %d\n", int_key);
 
+        readKey(int_key, K1, key_len);
+///        // scanf("%d" , &block_size);
 
-    plaintext = blockDecryptV4(ciphertext, K1, key_len, block_size);
-   // cout << "Decrypted plaintext: "<<plaintext << endl;
+        // printf("bLOCK SIZE: %d\n", block_size);
 
-    // printf("------------------------\n");
-    // printf("----Time taken: -- %.29f ms \n", (double)(clock() - tStart)*1000/CLOCKS_PER_SEC);
-    // printf("------------------------\n");
+        // cin >> plaintext;
+       ////// cout << "plaintext length:" << plaintext.length();
+      // cout << "Plaintext:" << plaintext << endl;
+        //printf("Plaintext: %s\n", plaintext);
 
-    printf("%.29f\n", (double)(clock() - tStart)*1000/CLOCKS_PER_SEC);
+        ciphertext = blockEncryptionV4 ( block_size ,  plaintext , K1, key_len);
+        cout << "Ciphertext: " <<  ciphertext << endl;
+
+        // ciphertext = "11111111111111111111111111111111111111111111";
+
+        plaintext = blockDecryptV4(ciphertext, K1, key_len, block_size);
+       cout << "Decrypted plaintext: "<<plaintext << endl;
+
+        // printf("------------------------\n");
+        // printf("----Time taken: -- %.29f ms \n", (double)(clock() - tStart)*1000/CLOCKS_PER_SEC);
+        // printf("------------------------\n");
+
+        printf("%.29f\n", (double)(clock() - tStart)*1000/CLOCKS_PER_SEC);
+    }
 
     return 0;
 }
